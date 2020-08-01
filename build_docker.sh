414d98bdf6dada5ffc8ff5101d9316ac22dbb555
Demian Holderegger <demian.holderegger@gmail.com>
2020-08-01 13:36:02 +0200

    image v2

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export VERSION=v1
+export VERSION=v2
 
 SHELLDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SHELLDIR
@@ -8,3 +8,8 @@ cd $SHELLDIR
 docker build . -t demianh/directus-app-backend:$VERSION --no-cache
 
 #docker push demianh/directus-app-backend:$VERSION
+
+echo -e ""
+echo -e "Run the following command to push the image:"
+echo -e "docker push demianh/directus-app-backend:$VERSION"
+echo -e ""

0ac3f5a1174121bbd98a295389577e807a6dcb79
Jonas Marty <jonas.marty@konplan.com>
2022-05-31 18:28:01 +0200

    :WIP: Authorization

---
+++ web/Mova21AppBackend/Mova21AppBackend.Data/Storage/DirectusPermissionRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using MoreLinq;
+using MoreLinq.Experimental;
+using Mova21AppBackend.Data.Interfaces;
+using Mova21AppBackend.Data.Models;
+using Mova21AppBackend.Data.RestModels;
+using RestSharp;
+
+namespace Mova21AppBackend.Data.Storage;
+
+public class DirectusPermissionRepository : BaseDirectusRepository, IPermissionRepository
+{
+    const string WeatherUrl = "items/Permission";
+
+    public DirectusPermissionRepository(IConfiguration configuration) : base(configuration)
+    {
+    }
+
+    public async Task<WeatherEntries> GetPermission()
+    {
+        var request = new RestRequest(WeatherUrl);
+        var response = await Client.ExecuteGetAsync<WeatherEntriesResponse>(request);
+
+        var permissionEntry = response.Data?.Data?.Single();
+    }
+}
